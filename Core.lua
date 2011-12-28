638d77b2d1774fe85f1ccc211623ff3d205784be
James FitzGibbon <james@nadt.net>
2011-12-27 23:04:56 -0800

    ...

--- Core.lua
+++ Core.lua
@@ -4,8 +4,38 @@
 
 NTL = LibStub("AceAddon-3.0"):NewAddon(
     "NADTTrustList",
-    "AceConsole-3.0",
+    "AceConsole-3.0"
 )
+
+function NTL:IsGroupTrusted()
+
+	local grouptrusted = false
+
+	if GetNumRaidMembers() > 0 then
+		for i = 1, GetNumRaidMembers() do
+			local name = UnitName("raid"..i, true)
+			if not self:IsUnitTrusted(name) then
+				return false
+			end
+		end
+	elseif GetNumPartyMembers() > 0 then
+		for i = 1, GetNumPartyMembers() do
+			local name = UnitName("party"..i, true)
+			if not self:IsUnitTrusted(name) then
+				return false
+			end
+		end
+	end
+	
+	return true
+
+end
+
+function NTL:IsUnitTrusted(name)
+
+	if NTL_DB.trusted[name] then return true else return false end
+
+end
 
 --
 -- EOF
